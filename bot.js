3982869e6e8b78452e21b27a5e9403fb1e8191a8
jrekier <jrekier@users.noreply.github.com>
2024-08-06 14:31:02 +0200

    add prefix variable

--- bot.js
+++ bot.js
@@ -8,9 +8,18 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
-  if (message.content === '!hello') {
-    message.reply('Hello! I am a Discord bot.');
+// Set the prefix
+let prefix = "!";
+client.on("messageCreate", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(`${prefix}ping`)) {
+    message.reply("pong!");
+  } else
+
+  if (message.content.startsWith(`${prefix}foo`)) {
+    message.reply("bar!");
   }
 });
 
