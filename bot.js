47514402a9893cb6197db95590e56e352a961340
jrekier <jrekier@users.noreply.github.com>
2024-08-06 13:16:22 +0200

    Init

---
+++ bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!hello') {
+    message.reply('Hello! I am a Discord bot.');
+  }
+});
+
+client.login(TOKEN);
